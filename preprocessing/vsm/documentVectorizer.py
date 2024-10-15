0e1ca75405108191801a7ddad5894c4f3b882622
Liam Leirs <liamleirs@hotmail.com>
2024-10-15 15:48:53 +0200

    Add function for calculating tf-idf weight of term

--- preprocessing/vsm/documentVectorizer.py
+++ preprocessing/vsm/documentVectorizer.py
@@ -1,5 +1,11 @@
 import numpy as np
+import math
 
 class documentVectorizer():
     def __init__(self):
         pass
+
+    def tf_idf_weight(self, tf: int, df: int, N: int) -> float:
+        tf_weight = 1 + math.log10(tf)
+        idf_weight = math.log10(N/df)
+        return tf_weight * idf_weight
