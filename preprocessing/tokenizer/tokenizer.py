3b31614eed4f616829221a009c2a39b8fa189d88
jarne-besjes <91670760+jarne-besjes@users.noreply.github.com>
2024-10-20 15:28:26 +0200

    Add extenstions to tokenizer

--- preprocessing/tokenizer/tokenizer.py
+++ preprocessing/tokenizer/tokenizer.py
@@ -1,3 +1,4 @@
+import nltk.corpus
 from nltk.tokenize import word_tokenize
 
 
@@ -39,13 +40,41 @@ class TokenStream:
 
 
 class Tokenizer:
+
     @staticmethod
-    def tokenize(file_path):
+    def _remove_stop_words(tokens: list[str]) -> list[str]:
+        """
+        Remove stop words from the list of tokens
+        :param tokens: The list of tokens to remove stop words from
+        :return: list[str]: The list of tokens with stop words removed
+        """
+        stop_words = set(nltk.corpus.stopwords.words('english'))
+        return [token for token in tokens if token not in stop_words]
+    @staticmethod
+    def tokenize(file_path, lower_case=True,  remove_stop_words=True, stemming = True, remove_punctuation_marks = True) -> TokenStream:
         """
         Tokenize the text in the file at the given path
         :param file_path: The path to the file to tokenize
+        :param lower_case: Whether to convert the tokens to lowercase
+        :param remove_stop_words: Whether to remove stop words from the tokens
+        :param stemming: Whether to apply stemming to the tokens
         :return: TokenStream: A stream of tokens from the file
         """
         with open(file_path, 'r', encoding='utf-8') as file:
             text = file.read()
-        return TokenStream(word_tokenize(text))
+            if lower_case:
+                text = text.lower()
+        tokens = word_tokenize(text)
+        if remove_stop_words:
+            tokens = Tokenizer._remove_stop_words(tokens)
+        if stemming:
+            stemmer = nltk.stem.PorterStemmer()
+            tokens = [stemmer.stem(token) for token in tokens]
+        if remove_punctuation_marks:
+            tokens = [token for token in tokens if token not in ['.', ',', '!', '?', ';', ':', '"', "'"]]
+        return TokenStream(tokens)
+
+if __name__ == "__main__":
+    stream = Tokenizer.tokenize('test.txt')
+    while stream.has_next():
+        print(stream.next())
