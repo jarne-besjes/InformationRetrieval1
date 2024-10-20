b35dda916611c2cbb6e34c290ee2dca8b48d1997
Liam Leirs <liamleirs@hotmail.com>
2024-10-20 12:58:10 +0200

    Normalized document vectors and saved in sparse format

--- preprocessing/vsm/documentVectorizer.py
+++ preprocessing/vsm/documentVectorizer.py
@@ -1,22 +1,15 @@
 import numpy as np
 import math
 from ..indexing.index_api import IndexApi
+from scipy import sparse
 
 class DocumentVectorizer:
     def __init__(self, index_path: str, out_path):
         self.indexAPI = IndexApi(index_path)
         self.out_path = out_path
 
-    def tf_idf_weight(self, tf: int, df: int, N: int) -> float:
-        # Returns the tf-idf weight of a term
-        if tf == 0:
-            return 0
-        tf_weight = 1 + math.log10(tf)
-        idf_weight = math.log10(N/df)
-        return tf_weight * idf_weight
-
     def compute_tf_vector(self, doc_id: int) -> np.array:
-        # Computes the vector for a single document
+        # Computes the tf vector for a single document
         vocab = self.indexAPI.get_sorted_vocabulary()
         T = len(vocab)
         tf_vector = np.empty(shape=(T,))
@@ -35,18 +28,30 @@ class DocumentVectorizer:
         T = len(vocab)
         N = len(doc_ids)
         doc_frequencies = np.empty(shape=(T, 1))
+        # Compute document frequencies and initialize
+        # matrix to N times idf vector
         for term_i, term in enumerate(vocab):
             postings_list = self.indexAPI.get_postings_list(term)
             df = postings_list.get_document_frequency()
             doc_frequencies[term_i] = math.log10(N/df)
         doc_matrix = np.tile(doc_frequencies, N)
-        print(doc_matrix)
+        # For each doc compute term frequencies and multiply
+        # tf vector element-wise with idf vector (= tf-idf)
         for doc_id in doc_ids:
             tf_vector = self.compute_tf_vector(doc_id)
             col = doc_matrix[:, doc_id-1]
             tf_idf_vector = np.multiply(col, tf_vector)
             doc_matrix[:, doc_id-1] = tf_idf_vector
-        np.save(self.out_path, doc_matrix)
+        # Normalize vectors
+        for doc_id in range(N):
+            col = doc_matrix[:, doc_id]
+            norm = np.linalg.norm(col, ord=2)
+            if norm != 0:
+                normalized = col/np.linalg.norm(col, ord=2)
+                doc_matrix[:, doc_id] = normalized
+        # Convert to sparse format
+        sparse_matrix = sparse.csr_matrix(doc_matrix)
+        sparse.save_npz("doc_vectors", sparse_matrix)
 
 if __name__ == "__main__":
     vectorizer = DocumentVectorizer("inverted_index", "doc_vectors")
