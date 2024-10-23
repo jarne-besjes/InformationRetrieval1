2911588ba3f08989fc2435f97f2d93927d6f4b6e
Liam Leirs <liamleirs@hotmail.com>
2024-10-23 13:27:07 +0200

    Changed to lil matrix format for quick element manipulations

--- preprocessing/vsm/documentVectorizer.py
+++ preprocessing/vsm/documentVectorizer.py
@@ -25,27 +25,27 @@ class DocumentVectorizer:
         doc_ids = self.indexAPI.get_document_ids()
         T = len(vocab)
         N = len(doc_ids)
-        doc_frequencies = np.zeros(shape=(T, 1))
+        doc_frequencies = np.zeros(shape=(T,))
         # Compute document frequencies and initialize
         # matrix to N times idf vector
         for term_i, term in enumerate(vocab):
             postings_list = self.indexAPI.get_postings_list(term)
             df = postings_list.get_document_frequency()
-            doc_frequencies[term_i, :] = math.log10(N/df)
-        doc_matrix = np.tile(doc_frequencies, N)
+            doc_frequencies[term_i] = math.log10(N/df)
+        doc_matrix = sparse.lil_matrix((T, N))
         # For each doc compute term frequencies and multiply
         # tf vector element-wise with idf vector (= tf-idf)
         for doc_id in doc_ids:
             tf_vector = self.compute_tf_vector(doc_id)
-            col = doc_matrix[:, doc_id-1]
-            tf_idf_vector = np.multiply(col, tf_vector)
+            tf_idf_vector = np.multiply(doc_frequencies, tf_vector)
             # Normalize vector
             norm = np.linalg.norm(tf_idf_vector, ord=2)
-            tf_idf_vector /= norm
-            doc_matrix[:, doc_id-1] = tf_idf_vector
+            if norm > 0:
+                tf_idf_vector /= norm
+            doc_matrix[:, doc_id-1] = tf_idf_vector.reshape(T, 1)
         # Convert to sparse format
-        sparse_matrix = sparse.csr_matrix(doc_matrix)
-        sparse.save_npz("doc_vectors", sparse_matrix)
+        sparse_matrix = doc_matrix.tocsr()
+        sparse.save_npz(self.out_path, sparse_matrix)
 
 if __name__ == "__main__":
     vectorizer = DocumentVectorizer("inverted_index", "doc_vectors")
